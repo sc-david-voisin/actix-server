9720381cc79e57c1efbc7c89e5822cdef2d192b5
David Voisin <david.voisin@scalingo.com>
2025-01-02 21:27:41 +0100

    read PORT from variable environment

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::env;
 
-const HOST: &str = "0.0.0.0";
-const PORT: u16 = 8080;
+const DEFAULT_HOST: &str = "0.0.0.0";
+const DEFAULT_PORT: u16 = 8080;
 
 async fn is_alive() -> impl Responder {
     HttpResponse::Ok().body("born to be alive!")
@@ -9,12 +10,17 @@ async fn is_alive() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let port: u16 = match env::var("PORT") {
+        Ok(p) => p.parse().unwrap_or(DEFAULT_PORT),
+        Err(_) => DEFAULT_PORT,
+    };
+
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(is_alive))
             .route("/status", web::get().to(is_alive))
     })
-    .bind((HOST, PORT))?
+    .bind((DEFAULT_HOST, port))?
     .run()
     .await
 }
